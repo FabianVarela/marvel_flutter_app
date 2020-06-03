acb03694233b4faa0d0def2f187fec3fe287d566
fabianvarela <jfabian.303@gmail.com>
2020-06-02 22:11:59 -0500

    Set store provider in main application to initialize redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:marvel_flutter_app/redux/reducer/main_reducer.dart';
+import 'package:marvel_flutter_app/redux/state/app_state.dart';
 import 'package:marvel_flutter_app/ui/list/marvel_list_ui.dart';
+import 'package:redux/redux.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,10 +12,19 @@ void main() async {
   await SystemChrome.setPreferredOrientations(
       <DeviceOrientation>[DeviceOrientation.portraitUp]);
 
-  runApp(MyApp());
+  final Store<AppState> store = Store<AppState>(
+    appReducer,
+    initialState: AppState.initialState(),
+  );
+
+  runApp(MyApp(store: store));
 }
 
 class MyApp extends StatefulWidget {
+  final Store<AppState> store;
+
+  MyApp({@required this.store});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -19,9 +32,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Marvel Superheroes',
-      home: MarvelListUI(),
+    return StoreProvider<AppState>(
+      store: widget.store,
+      child: MaterialApp(
+        title: 'Flutter Marvel Superheroes',
+        home: MarvelListUI(),
+      ),
     );
   }
 }
